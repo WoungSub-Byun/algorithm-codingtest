3d31a5b87edad0ab22248ec8f2b9e773dfa90302
icefirebear <woungsub1234@gmail.com>
2020-12-26 16:52:54 +0900

    Add 위장2

--- programmers/위장.py
+++ programmers/위장.py
@@ -1,3 +1,4 @@
+# 첫번째 풀이
 from collections import Counter
 def solution(clothes):
     items = dict(Counter([i[1] for i in clothes]))
@@ -6,3 +7,10 @@ def solution(clothes):
     for i in items.values():
         cnt *= (i+1)
     return cnt - 1
+
+# 두번째 풀이
+def solution(clothes):
+    from collections import Counter
+    from functools import reduce
+    items = Counter([i[1] for i in clothes])
+    return reduce(lambda x, y: x*(y+1), items.values(),1) -1
