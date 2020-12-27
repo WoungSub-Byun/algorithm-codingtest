dbe567d9e73f8e762de00d22b225e3fe3bf68c4e
icefirebear <woungsub1234@gmail.com>
2020-12-27 16:31:49 +0900

    Add 체육복 2

--- programmers/체육복.py
+++ programmers/체육복.py
@@ -15,3 +15,15 @@ def solution(n, lost, reserve):
             break
     return can_class
 
+# 두번째 풀이
+def solution(n, lost, reserve):
+    _reserve = [r for r in reserve if r not in lost]
+    _lost = [l for l in lost if l not in reserve]
+
+    for r in _reserve:
+        if r - 1 in _lost:
+            _lost.remove(r - 1)
+        elif r + 1 in _lost:
+            _lost.remove(r + 1)
+
+    return n - len(_lost)
