4895e748f9a848b9adf730386bff157b3bbc1393
icefirebear <woungsub1234@gmail.com>
2020-12-28 15:12:54 +0900

    Add 문자열 재정렬

---
+++ thisis-coddingtest/Implementation/문자열 재정렬.py
@@ -0,0 +1,19 @@
+def solution():
+    input_str = list(input())
+    char_list = [i for i in input_str if ord(i) >= 64]
+    char_list.sort()
+    num = sum([int(i) for i in input_str if ord(i) < 64])
+    return ''.join(char_list)+str(num)
+
+def solution():
+    input_str = list(input())
+    num = 0
+    result = []
+    for i in input_str:
+        if i.isalpha():
+            result.append(i)
+        else:
+            num += int(i)
+    result.sort()
+    return ''.join(result) + str(num)
+
