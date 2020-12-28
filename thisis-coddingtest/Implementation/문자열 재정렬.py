478c7ac22136d8667075a73d662b986ca5f0c754
icefirebear <woungsub1234@gmail.com>
2020-12-28 15:13:48 +0900

    Update

--- thisis-coddingtest/Implementation/문자열 재정렬.py
+++ thisis-coddingtest/Implementation/문자열 재정렬.py
@@ -1,3 +1,4 @@
+# 첫번쨰 풀이
 def solution():
     input_str = list(input())
     char_list = [i for i in input_str if ord(i) >= 64]
@@ -5,6 +6,7 @@ def solution():
     num = sum([int(i) for i in input_str if ord(i) < 64])
     return ''.join(char_list)+str(num)
 
+# 두번쨰 풀이
 def solution():
     input_str = list(input())
     num = 0
