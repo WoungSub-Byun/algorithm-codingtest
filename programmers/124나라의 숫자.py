fb252996b1eda052dfb55029947d00d93c9ad770
icefirebear <woungsub1234@gmail.com>
2021-01-07 22:06:46 +0900

    Add 124나라의 숫자2

--- programmers/124나라의 숫자.py
+++ programmers/124나라의 숫자.py
@@ -8,3 +8,11 @@ def solution(n):
     answer += steps[n % 3]
     return answer[::-1]
 
+
+# 두번째 풀이
+def solution(n):
+    if n <= 3:
+        return '124'[n - 1]
+    else:
+        q, r = divmod(n - 1, 3)
+        return solution(q) + '124'[r]
