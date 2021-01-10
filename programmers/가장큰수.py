a56a2d38eef865250c5e52547589400ab2bf3267
icefirebear <woungsub1234@gmail.com>
2021-01-10 17:04:20 +0900

    Add 가장큰수2

--- programmers/가장큰수.py
+++ programmers/가장큰수.py
@@ -6,3 +6,10 @@ def solution(numbers):
 # permutations로 모든 조합을 만들어내고 리스트로 바꾸고 각각을 join하고 그 리스트에서 최댓값을 찾아내는... 단순한 풀이법
 # 테스트케이스는 맞았지만 시간초과로 0점이었다.    
 
+
+# 두번째 풀이 - 100
+def solution(numbers):
+    if len(numbers) == numbers.count(0):
+        return '0'
+    numbers.sort(key=lambda x: str(x) * 3, reverse=True)
+    return ''.join(list(map(str, numbers)))
