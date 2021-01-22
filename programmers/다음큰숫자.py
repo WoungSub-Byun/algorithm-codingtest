a5fce6ac20d6d0311e3a20c8edc72a2beee529dc
icefirebear <woungsub1234@gmail.com>
2021-01-22 10:07:56 +0900

    Add 다음큰숫자2

--- programmers/다음큰숫자.py
+++ programmers/다음큰숫자.py
@@ -5,3 +5,13 @@ def solution(n):
         if bin(n).count("1") == bin(answer).count("1"):
             return answer
         answer += 1
+
+
+# 2차 풀이 100
+def solution(n):
+    answer = n + 1
+    n = bin(n).count("1")
+    while True:
+        if n == bin(answer).count("1"):
+            return answer
+        answer += 1
