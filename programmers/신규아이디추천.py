4ca1416470fefae8b3c2f17af7670f28a7f87f60
변웅섭 <woungsub1234@gmail.com>
2021-04-20 15:14:38 +0900

    Add 신규아이디추천

--- programmers/신규아이디추천.py
+++ programmers/신규아이디추천.py
@@ -19,3 +19,24 @@ def solution(new_id):
         while len(new_id) != 3:
             new_id += new_id[-1]
     return new_id
+
+
+# 2차 시도
+import re
+
+
+def solution(new_id):
+    new_id = new_id.lower()
+    c2 = re.compile("[^a-z0-9-_.]")
+    listed_id = list(new_id)
+    for x in c2.findall(new_id):
+        del listed_id[listed_id.index(x)]
+
+    new_id = re.sub("[.]+", ".", "".join(listed_id)).strip(".")
+    if not new_id:
+        new_id = "a"
+    if len(new_id) >= 16:
+        new_id = new_id[:15].rstrip(".")
+    if len(new_id) <= 2:
+        new_id += new_id[-1] * (3 - len(new_id))
+    return new_id
