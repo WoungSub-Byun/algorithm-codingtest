9d6e09c90f78eaa5241e07d547f66f8334bb4da8
변웅섭 <woungsub1234@gmail.com>
2021-04-20 15:17:19 +0900

    add 신규아이디추천

--- programmers/신규아이디추천.py
+++ programmers/신규아이디추천.py
@@ -40,3 +40,16 @@ def solution(new_id):
     if len(new_id) <= 2:
         new_id += new_id[-1] * (3 - len(new_id))
     return new_id
+
+
+# 3차 시도: 최대한 정규식을 사용하여 풀이
+import re
+
+
+def solution(new_id):
+    new_id = new_id.lower()
+    new_id = re.sub("[^a-z0-9-_.]", "", new_id)
+    new_id = re.sub("\.+", ".", new_id).strip(".")
+    new_id = "a" if len(new_id) == 0 else new_id[:15].rstrip(".")
+    new_id = new_id if len(new_id) > 2 else new_id + new_id[-1] * (3 - len(new_id))
+    return new_id
