e752d7fa2aead30f98e7b891d7846ea0489ccd35
변웅섭 <woungsub1234@gmail.com>
2021-04-30 00:11:38 +0900

    Add 방문길이 풀이

--- programmers/방문길이.py
+++ programmers/방문길이.py
@@ -58,7 +58,10 @@ def solution(dirs):
 def solution(dirs):
 
     x, y = 0, 0
-    new_dirs, visited = list(), list()
+    new_dirs, visited = (
+        list(),
+        list(),
+    )  # new_dir: 문자를 이동방향으로 바꾸어준 리스트, visited: 지나간 경로를 확인하기 위한 리스트
 
     for dir in dirs:
         if dir == "U":
@@ -71,16 +74,21 @@ def solution(dirs):
             new_dirs.append([0, -1])
 
     for dir in new_dirs:
-        prev = (x, y)
+        prev = (x, y)  # 이전 좌표
         x += dir[0]
         y += dir[1]
-        if x < -5 or x > 5 or y < -5 or y > 5:
+        if (
+            x < -5 or x > 5 or y < -5 or y > 5
+        ):  # 맵의 길이가 (-5, 5)이기 때문에 이를 넘어가게 된다면 이전에 더했던 이동방향을 다시 빼준다.
             x += -dir[0]
             y += -dir[1]
             continue
-        elif [prev, (x, y)] in visited or [(x, y), prev] in visited:
+        elif [prev, (x, y)] in visited or [
+            (x, y),
+            prev,
+        ] in visited:  # 맵을 이탈하지 않았다면 이미 지나간 경로인지 visited리스트를 통해 확인한다.
             continue
-        visited.append([prev, (x, y)])
+        visited.append([prev, (x, y)])  # 위의 두 조건문에 걸리지 않았다면 이동 경로를 visited에 추가해준다.
     return len(visited)
 
 
