e196f102dca257fac3b8ad428fcca41a643acfd1
변웅섭 <woungsub1234@gmail.com>
2021-04-30 00:04:27 +0900

    Add 방문길이 2

--- programmers/방문길이.py
+++ programmers/방문길이.py
@@ -23,3 +23,32 @@ def solution(dirs):
             continue
         visited.append((x, y))
     return len(visited)
+
+
+# 이전 좌표까지 넣었지만 경로의 이동방향에 따라 두가지 경우로 나누지 않기 때문에 현재 좌표에서 이전 좌표로가는 경로도 추가해주어야 한다.
+def solution(dirs):
+
+    x, y = 0, 0
+    new_dirs, visited = list(), list()
+    for dir in dirs:
+        if dir == "U":
+            new_dirs.append([0, 1])
+        elif dir == "L":
+            new_dirs.append([-1, 0])
+        elif dir == "R":
+            new_dirs.append([1, 0])
+        elif dir == "D":
+            new_dirs.append([0, -1])
+
+    for dir in new_dirs:
+        prev = (x, y)
+        x += dir[0]
+        y += dir[1]
+        if x < -5 or x > 5 or y < -5 or y > 5:
+            x += -dir[0]
+            y += -dir[1]
+            continue
+        elif [prev, (x, y)] in visited:
+            continue
+        visited.append([prev, (x, y)])
+    return len(visited)
