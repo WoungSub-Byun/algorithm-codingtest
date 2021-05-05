67e90dcf17dcc2ad10db1610934df7c008529b98
변웅섭 <woungsub1234@gmail.com>
2021-05-05 14:06:52 +0900

    Add 로또의 최고순위와 최저 순위2

--- programmers/로또의 최고순위와 최저순위.py
+++ programmers/로또의 최고순위와 최저순위.py
@@ -7,3 +7,16 @@ def solution(lottos, win_nums):
     min_rank = 6 if min_num < 2 else 7 - min_num
     max_rank = 7 - max_num
     return [max_rank, min_rank]
+
+
+# 다른 사람들 풀이 중 참신한 방법
+def other_solution(lottos, win_nums):
+
+    rank = [6, 6, 5, 4, 3, 2, 1]
+
+    cnt_0 = lottos.count(0)
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+    return rank[cnt_0 + ans], rank[ans]
